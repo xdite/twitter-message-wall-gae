4b2596afd8122cabcfcf1fff4badecb4227b9754
xdite <xdite@handlino.com>
2009-04-19 15:27:09 +0800

    fix tweet_id bug

--- app.rb
+++ app.rb
@@ -14,8 +14,8 @@ get '/' do
   unless params[:page]  
     @results.each do |result|
         tweet = Tweet.find(:tweet_id => result.id )
-        unless tweet
-          tweet = Tweet.create(:text => result.text ,:from_user  => result.from_user,:to_user =>result.to_user,:from_user_id =>  result.from_user_id,:to_user_id => result.to_user_id,:created_at  => result.created_at ,:profile_image_url =>result.profile_image_url,:iso_language_code =>result.iso_language_code)
+        unless tweet 
+          tweet = Tweet.create(:tweet_id => result.id , :text => result.text ,:from_user  => result.from_user,:to_user =>result.to_user,:from_user_id =>  result.from_user_id,:to_user_id => result.to_user_id,:created_at  => result.created_at ,:profile_image_url =>result.profile_image_url,:iso_language_code =>result.iso_language_code)
         end
      end
   end
