327e4aa8c4a0c2ac483189b11838b400bf70250c
xdite <xdite@handlino.com>
2009-04-19 10:59:48 +0800

    add unless params

--- app.rb
+++ app.rb
@@ -10,14 +10,15 @@ require 'lib/view_helpers'
 get '/' do
   @client = TwitterSearch::Client.new 'politweets'
   @results = @client.query :q => '@MrIE6'
-    
-   @results.each do |result|
-      tweet = Tweet.find(:tweet_id => result.id )
-      unless tweet
-        tweet = Tweet.create(:text => result.text ,:from_user  => result.from_user,:to_user =>result.to_user,:from_user_id =>  result.from_user_id,:to_user_id => result.to_user_id,:created_at  => result.created_at ,:profile_image_url =>result.profile_image_url,:iso_language_code =>result.iso_language_code)
-      end
-   end
   
+  unless params[:page]  
+    @results.each do |result|
+        tweet = Tweet.find(:tweet_id => result.id )
+        unless tweet
+          tweet = Tweet.create(:text => result.text ,:from_user  => result.from_user,:to_user =>result.to_user,:from_user_id =>  result.from_user_id,:to_user_id => result.to_user_id,:created_at  => result.created_at ,:profile_image_url =>result.profile_image_url,:iso_language_code =>result.iso_language_code)
+        end
+     end
+  end
   @tweets = Tweet.all.paginate(:page => params[:page], :per_page => 10)
   
   erb :home
